cbf56dceef996adeee4fc7c5d2d6f33ef039d71b
dimassulz <dimassulz@gmail.com>
2022-05-18 21:37:39 -0300

    add cadastro medico

--- laboratorio/models/tbMedico.php
+++ laboratorio/models/tbMedico.php
@@ -1,13 +1,29 @@
 <?php
-$sql = "SELECT co_medico, nu_crm, no_medico
+function buscarTodosMedicos($conn){
+    $sql = "SELECT co_medico, nu_crm, no_medico
     FROM tb_medico 
     ORDER BY no_medico";
 
-$stmt = $conn->prepare($sql); //stmt statement (prepara)
-$stmt->execute(); //executar o sql
-$tbMedico = $stmt->fetchAll(); // trazer todos os dados do sql no formato de array
+    $stmt = $conn->prepare($sql); //stmt statement (prepara)
+    $stmt->execute(); //executar o sql
+    return $stmt->fetchAll(); // trazer todos os dados do sql no formato de array
+}
 
-$conn = null; // libero a conexão (disconnect)
+function insertMedico($conn, $dados){
+    $sql = "INSERT INTO tb_medico (nu_crm, no_medico) VALUES (:nu_crm, :no_medico)";
+    $stmt= $conn->prepare($sql);
+    return $stmt->execute($dados);
+}
 
+function editMedico($conn, $dados){
+    $sql = "UPDATE tb_medico SET nu_crm = :nu_crm, no_medico = :no_medico WHERE co_medico = :co_medico";
+    $stmt= $conn->prepare($sql);
+    return $stmt->execute($dados);
+}
 
+function deleteMedico($conn,$id){
+    $sql = "DELETE FROM tb_medico WHERE co_medico = :co_medico";
+    $stmt= $conn->prepare($sql);
+    return $stmt->execute(['co_medico' => $id]);
+}
 
